d38a9844e9f73836f1ce9495c704b3b67593b764
EihabAhmed <eihabahmed@gmail.com>
2023-05-31 23:44:53 +0300

    Added assets and common files

---
+++ core/src/com/mygdx/rectangledestroyerch10/Brick.java
@@ -0,0 +1,10 @@
+package com.mygdx.rectangledestroyerch10;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Brick extends BaseActor {
+    public Brick(float x, float y, Stage s) {
+        super(x, y, s);
+        loadTexture("brick-gray.png");
+    }
+}
---
+++ core/src/com/mygdx/rectangledestroyerch10/Wall.java
@@ -0,0 +1,14 @@
+package com.mygdx.rectangledestroyerch10;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Wall extends BaseActor {
+    public Wall(float x, float y, float width, float height, Stage s) {
+        super(x, y, s);
+        loadTexture("white-square.png");
+        setSize(width, height);
+        setColor(Color.GRAY);
+        setBoundaryRectangle();
+    }
+}
